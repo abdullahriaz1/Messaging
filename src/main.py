a29a33f4b14b52a1e70a5a6a75fbea1b11c909ce
Abdullah Riaz <ariaz988@gmail.com>
2022-01-13 14:35:35 -0800

    changes

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from handler import Handler
 def main():
     s = Handler()
     while True:
-        
+        pass
 #create communication between users: one person creates a chat link. creates chat in server with unique identifier (URL)
 #anyone with the identifier can join the chat. chat automatically deletes within one hour
 
